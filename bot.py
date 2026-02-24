3b011744677d8dd9e34d94fc7cb36f588d046dcf
MMTEMN <marlokeadam@gmail.com>
2026-02-24 21:13:16 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,33 @@
+import discord
+from bot_mantik import yazi_tura, gen_pass,saat 
+
+
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+intents = discord.Intents.default()
+# Mesajları okuma ayrıcalığını etkinleştirelim
+intents.message_content = True
+# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} olarak giriş yaptık.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$merhaba'):
+        await message.channel.send("Selam!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    elif message.content.startswith('$sifre'):
+        await message.channel.send("Sifreniz: " + gen_pass(10))
+    elif message.content.startswith('$oyun'):
+        await message.channel.send(yazi_tura())
+    elif message.content.startswith('$saat'):
+        await message.channel.send("su anda saat:"+saat())
+    else:
+        await message.channel.send(message.content)
+
+client.run("MTQ3NDExMTIzMzQ1MDgzNjE1MQ.G-VZov.xnzT7UqIQf8plasN7FBQOyzhIxroKpXQXco1bk")
---
+++ bot_mantik.py
@@ -0,0 +1,23 @@
+import random
+import time
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#514988?=@<>abcdefghijklmnopqrstwyzx"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+print(gen_pass(10))
+
+def yazi_tura():
+    para=random.randint(0,2)
+    if para == 0:
+        return "YAZI"
+    else:
+        return "TURA" 
+    
+def saat():
+    return time.strftime ( " %H:%M:%S" ) ## 24 saat formatı ## time.strftime ( " %H:%M:%S" ) ## 12 saat formatı ## time.strftime ( "%I:%M: % S " )
